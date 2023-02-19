58240bd2472f0d6006e03d4cf9105421ad48b998
ik04 <63468587+ik04@users.noreply.github.com>
2023-02-19 16:40:01 +0530

    home page layout designing

--- site/pages/home.js
+++ site/pages/home.js
@@ -15,7 +15,23 @@ const home = () => {
   return (
     <div>
       <HomeNavbar />
-      <p>{userRealname}</p>
+      <div className="sidebar flex flex-col bg-gray-200 w-72 h-screen space-y-28 fixed">
+        <h1 className="font-mono mx-20  text-black h-14 p-1 w-14 mt-4  text-3xl cursor-pointer">
+          Mastery
+        </h1>
+        <button className="bg-black hover:bg-white hover:text-black transition text-white font-bold mt-20 py-5 px-4 mx-5 rounded-full">
+          Home
+        </button>
+        <button className="bg-black hover:bg-white hover:text-black transition text-white font-bold py-5 px-4 mx-5 rounded-full">
+          Search
+        </button>
+        <button className="bg-black hover:bg-white hover:text-black transition text-white font-bold py-5 px-4 mx-5 rounded-full">
+          Create
+        </button>
+        <button className="bg-black hover:bg-white hover:text-black transition text-white font-bold py-5 px-4 mx-5 rounded-full">
+          Settings
+        </button>
+      </div>
     </div>
   )
 }
