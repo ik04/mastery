e5568bb4822063b151959f88caeb09cdeb1360ab
ik04 <63468587+ik04@users.noreply.github.com>
2023-02-27 01:48:29 +0530

    home redirection added

--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -3,6 +3,7 @@ import React, { useContext, useEffect } from "react"
 import HomeNavbar from "@/components/HomeNavbar"
 import Sidebar from "@/components/Sidebar"
 import Card from "@/components/Card"
+import axios from "axios"
 
 const home = () => {
   // const {
@@ -26,6 +27,31 @@ const home = () => {
 }
 
 export default home
+export async function getServerSideProps(context) {
+  const url = "http://localhost:8000/api/user"
+  const cookie = context.req.cookies.at
+  const resp1 = await axios.get(url, { headers: { Cookie: `at=${cookie}` } })
+  axios.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${resp1.data.access_token}`
+  const email = resp1.data.email
+
+  try {
+    const instance = axios.create({
+      withCredentials: true,
+    })
+    const url = "http://localhost:8000/api/isLog"
+    const resp = await instance.post(url, {})
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    }
+  }
+  return { props: {} }
+}
 
 /*
 TODO:
